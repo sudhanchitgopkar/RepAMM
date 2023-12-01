2b00874bc60922eb024a383a5d15fa063c71cb31
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-11-30 22:58:03 -0500

    AMM -> abstract

--- src/AMM.java
+++ src/AMM.java
@@ -1,5 +1,11 @@
 package mm;
 
-public class AMM {
+import infra.Agent;
+
+abstract public class AMM {
     
+    abstract void buy(Agent buyer, double amt, int outcome);
+    abstract void sell(Agent seller, double amt, int outcome);
+    abstract double getPrice(int outcome);
+
 } //AMM
