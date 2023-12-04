9bb9de8e3b71ad7a6b503f05490eb906f6e9ad78
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-12-03 21:19:45 -0500

    🔧 added buyTill, sellTill, functions now public

--- src/AMM.java
+++ src/AMM.java
@@ -4,8 +4,10 @@ import infra.Agent;
 
 abstract public class AMM {
     
-    abstract double buy(Agent buyer, double amt, int outcome);
-    abstract double sell(Agent seller, double amt, int outcome);
-    abstract double getPrice(int outcome);
+    public abstract boolean buy(Agent buyer, double amt, int outcome);
+    public abstract boolean sell(Agent seller, double amt, int outcome);
+    public abstract double getPrice(int outcome);
+    public abstract double buyTillPrice(Agent a, int outcome, double price);
+    public abstract double sellTillPrice(Agent a, int outcome, double price);
 
 } //AMM
