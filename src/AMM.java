d06c635b30ddbd23b82569b745d099299bb89d19
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-12-05 20:41:23 -0500

    🔧 buyTillPrice allows Exceptions to be thrown

--- src/AMM.java
+++ src/AMM.java
@@ -7,7 +7,7 @@ abstract public class AMM {
     public abstract boolean buy(Agent buyer, double amt, int outcome);
     public abstract boolean sell(Agent seller, double amt, int outcome);
     public abstract double getPrice(int outcome);
-    public abstract double buyTillPrice(Agent a, int outcome, double price);
+    public abstract double buyTillPrice(Agent a, int outcome, double price) throws Exception;
     public abstract double sellTillPrice(Agent a, int outcome, double price);
 
 } //AMM
