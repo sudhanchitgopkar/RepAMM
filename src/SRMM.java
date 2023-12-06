21ca1a11d03f8b3a5d1d459469f5edaf251a8362
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-12-05 20:53:08 -0500

    🔧 buyTillPrice bug fixes

--- src/SRMM.java
+++ src/SRMM.java
@@ -138,24 +138,13 @@ public class SRMM extends AMM {
      */
     public double buyTillPrice(Agent a, int outcome, double price) throws Exception {
 	double qty = 0;
-	
-	for (int i = 0; i < state.length; i++) {
-	    if (i != outcome) {
-		qty += price * Math.exp(state[i]/BETA);
-	    } //if
-	} //for
-	qty /= (1 - price);
-	qty -= state[outcome];
-	
+	double x = Math.exp((price/BETA) + (state[outcome]/BETA));
+	double y = Math.exp((price/BETA) + (state[outcome == 0 ? 1 : 0]/BETA));
+	double z = Math.exp(state[outcome == 0 ? 1 : 0]/BETA);
+	qty = BETA * Math.log(x + y + z) - state[outcome];
+  
 	if (!this.buy(a, qty, outcome)) {
-	    //Buy as much as possible with remaining budget. Specifically for binary outcome!
-	    double x = Math.exp((price/BETA) + (state[outcome]/BETA));
-	    double y = Math.exp((price/BETA) + (state[outcome == 0 ? 1 : 0]/BETA));
-	    double z = Math.exp(state[outcome == 0 ? 1 : 0]/BETA);
-	    qty = BETA * Math.log(x + y + z) - state[outcome];
-	    if (!buy(a, qty, outcome)) {
-		throw new Exception("BUY TILL PRICE FALLBACK ERROR, TRIED TO BUY " + qty + " CONTRACTS");
-	    } //if
+		throw new Exception("BUY TILL PRICE ERROR, TRIED TO BUY " + qty + " CONTRACTS");
 	} //if
 
 	return qty;
