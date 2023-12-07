cd9f36b67a656e4ea92286f5439d55c5d3a78aeb
Sudhan Chitgopkar <sudhanchitgopkar@gmail.com>
2023-12-06 23:11:04 -0500

    🔧 default to running TFMM

--- src/Runner.java
+++ src/Runner.java
@@ -2,12 +2,14 @@ package infra;
 
 import mm.AMM;
 import mm.SRMM;
+import mm.TradeFeeMM;
 import infra.Sim;
 
 public class Runner {
     public static void main (String [] args) {
-	AMM srmm = new SRMM(2);
-	Sim s = new Sim(srmm);
+	//AMM srmm = new SRMM(2);
+	AMM tfmm = new TradeFeeMM(2); 
+	Sim s = new Sim(tfmm);
 	s.run();
     } //main
 } //class
